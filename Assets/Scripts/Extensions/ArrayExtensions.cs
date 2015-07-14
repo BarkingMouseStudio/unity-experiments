6072d26527c8c2f80eef0f96fd0d007af70b16bd
Jim Fleming <jimmy.fleming@gmail.com>
2015-07-14 10:05:13 -0700

    Adding shuffle to array

---
+++ Assets/Scripts/Extensions/ArrayExtensions.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ArrayExtensions {
+
+  public static void Shuffle<T>(this T[] arr) {
+    for (int i = 0; i < arr.Length; i++) {
+      arr.Swap(i, UnityEngine.Random.Range(i, arr.Length));
+    }
+  }
+
+  public static void Swap<T>(this T[] arr, int i, int j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+}
