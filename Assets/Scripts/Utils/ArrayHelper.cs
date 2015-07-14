462f6149b24ee28240c2976257fc28f4b5602d06
Jim Fleming <jimmy.fleming@gmail.com>
2015-07-14 10:08:27 -0700

    Removing stringify helper in favor of IEnumerable extension

--- Assets/Scripts/Utils/ArrayHelper.cs
+++
@@ -1,8 +0,0 @@
-using System.Linq;
-
-public static class ArrayHelper {
-
-  public static string Stringify<T>(T[] arr) {
-    return string.Join(",", arr.Select(v => v.ToString()).ToArray());
-  }
-}
