f34b91d871231fa43727798a80508f197ecbf9be
Jim Fleming <jimmy.fleming@gmail.com>
2015-07-15 16:27:33 -0700

    Improving NetworkIO tests

--- Assets/Scripts/NEAT/Editor/Tests/Phenotypes/NetworkIOTests.cs
+++ Assets/Scripts/NEAT/Editor/Tests/Phenotypes/NetworkIOTests.cs
@@ -49,10 +49,41 @@ namespace NEAT {
     }
 
     [Test]
+    [Ignore]
     public void TestNetworkIO_Send() {
       var genotype = Genotype.FromPrototype(protoGenotype);
       var network = NetworkIO.FromGenotype(genotype);
       network.Send(90.0f, 30.0f, 3.0f, 1.0f);
+    }
+
+    [Test]
+    public void TestNetworkIO_PopulateWorldData() {
+      var worldData = new float[NetworkIO.inNeuronCount];
+      NetworkIO.PopulateWorldData(worldData, 1.0f, 2.0f, 3.0f, 4.0f);
+      Assert.AreEqual("1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4",
+        worldData.Stringify());
+    }
+
+    [Test]
+    public void TestNetworkIO_MapInput() {
+      var worldData = new float[NetworkIO.inNeuronCount];
+      var input = new double[NetworkIO.InitialNeuronCount];
+      NetworkIO.PopulateWorldData(worldData, 1.0f, 2.0f, 3.0f, 4.0f);
+
+      NetworkIO.MapInput(input, worldData);
+      Assert.AreEqual("0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0",
+        input.Stringify());
+    }
+
+    [Test]
+    public void TestNetworkIO_MapOutput() {
+      var output = new double[NetworkIO.InitialNeuronCount];
+      output.Fill(30.0f);
+
+      var speed = NetworkIO.MapOutput(output);
+      Assert.AreEqual(0.0f, speed); // Speeds cancel eachother out
+      Assert.AreEqual("30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30",
+        output.Stringify());
     }
   }
 }
