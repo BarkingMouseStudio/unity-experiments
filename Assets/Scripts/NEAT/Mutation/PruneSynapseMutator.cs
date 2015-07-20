6ef4c30b9a61ef9cf87debfda753b455e30b5c7d
Jim Fleming <jimmy.fleming@gmail.com>
2015-07-20 09:40:49 -0700

    First pass at prune mutator (does not work yet)

--- Assets/Scripts/NEAT/Mutation/PruneSynapseMutator.cs
+++ Assets/Scripts/NEAT/Mutation/PruneSynapseMutator.cs
@@ -1,26 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace NEAT {
 
   public class PruneSynapseMutator : IMutator {
 
-    // float p;
+    float p;
 
     public PruneSynapseMutator(float p) {
-      // this.p = p;
+      this.p = p;
     }
 
     public void Mutate(Genotype genotype, MutationResults results) {
-      // var disabledSynapses = genotype.SynapseGenes
-      //   .Where(g => !g.isEnabled)
-      //   .Where(_ => Random.value < p);
-      // var synapseGenes = genotype.SynapseGenes.Except(disabledSynapses);
-      // genotype.SynapseGenes = synapseGenes.ToArray();
-      //
-      // var orphanedNeurons = genotype.NeuronGenes;
-      // var neuronGenes = genotype.NeuronGenes.Except(orphanedNeurons);
-      // genotype.NeuronGenes = neuronGenes.ToArray();
-      //
-      // results.orphanedNeurons += orphanedNeurons.Count;
-      // results.prunedSynapses += prunedSynapses.Count;
+      var prunedSynapses = genotype.SynapseGenes
+        .Where(g => !g.isEnabled)
+        .Where(_ => Random.value < p);
+
+      var synapseGenes = genotype.SynapseGenes.Except(prunedSynapses);
+      genotype.SynapseGenes = new GeneList<SynapseGene>(synapseGenes);
+
+      // An orphan is a neuron with no connecting synapses
+      // For each neuron: check if any synapse connects to it
+      // Exclude IO neurons
+      var orphanedNeurons = genotype.NeuronGenes.Skip(NetworkIO.InitialNeuronCount)
+        .Where(n => {
+          var hasConnections = synapseGenes.Any(s =>
+            s.fromNeuronId == n.InnovationId ||
+            s.toNeuronId == n.InnovationId);
+          return !hasConnections;
+        });
+      var neuronGenes = genotype.NeuronGenes.Except(orphanedNeurons);
+      genotype.NeuronGenes = new GeneList<NeuronGene>(neuronGenes);
+
+      results.orphanedNeurons += orphanedNeurons.Count();
+      results.prunedSynapses += prunedSynapses.Count();
     }
   }
 }
