f5713764601421083fc1538ea93e63c411248425
Jim Fleming <jimmy.fleming@gmail.com>
2015-07-21 20:58:21 -0700

    Making EliteSelector more flexible and using adjusted fitness

--- Assets/Scripts/NEAT/Selection/EliteSelector.cs
+++ Assets/Scripts/NEAT/Selection/EliteSelector.cs
@@ -9,10 +9,17 @@ namespace NEAT {
   public class EliteSelector : ISelector {
 
     public Genotype[] Select(Specie[] species, int eliteCount) {
-      var elites = species.Select(sp =>
-        sp.OrderBy(pt => pt.Fitness).First().Genotype).ToArray();
-      Assert.AreEqual(elites.Length, eliteCount,
+      var orderedSpecies = species.OrderByDescending(sp =>
+        sp.MeanAdjustedFitness);
+
+      var elites = orderedSpecies.Select(sp => {
+        var orderedSpecie = sp.OrderByDescending(pt => pt.AdjustedFitness);
+        return new Genotype(orderedSpecie.First().Genotype);
+      }).Take(eliteCount).ToArray();
+
+      Assert.AreEqual(eliteCount, elites.Length,
         "Must return the expected number of elites");
+
       return elites;
     }
   }
