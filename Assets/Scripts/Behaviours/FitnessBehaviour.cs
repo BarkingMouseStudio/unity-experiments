54d7f4e52f7e68a528992a189274497f91497f81
Jim Fleming <jimmy.fleming@gmail.com>
2015-08-10 16:14:07 -0700

    Adding a few output scripts for better visualization of the pendulum

---
+++ Assets/Scripts/Behaviours/FitnessBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FitnessBehaviour : MonoBehaviour {
+
+  public Text textField;
+
+  EvaluationBehaviour evaluation;
+
+	void Awake() {
+    textField.text = (0.0f).ToString();
+    evaluation = GetComponent<EvaluationBehaviour>();
+	}
+
+	void Update() {
+    if (evaluation.Phenotype != null) {
+      textField.text = evaluation.Phenotype.CurrentTrial.Fitness.ToString();
+    }
+	}
+}
---
+++ Assets/Scripts/Behaviours/SpeedBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBehaviour : MonoBehaviour {
+
+	public SpriteRenderer tile;
+	ControllerBehaviour controller;
+
+	void Awake() {
+		controller = GetComponent<ControllerBehaviour>();
+	}
+
+	static Color GetColor(double rate) {
+		if (rate >= 0.0f) {
+    	return Color.Lerp(Color.white, Color.red, (float)rate);
+		} else {
+    	return Color.Lerp(Color.white, Color.blue, Mathf.Abs((float)rate));
+		}
+	}
+
+	void Update() {
+		tile.color = GetColor(controller.Speed / 800.0f);
+	}
+}
